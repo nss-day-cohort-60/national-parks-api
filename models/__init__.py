720833c31f35a2ad36545cdc2c4235c90831b72f
Hazel Preza <hazel.d.preza@gmail.com>
2023-01-23 15:26:59 -0600

    added blogs to dunder init

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
 from .wildlife import Wildlife
 from .natural_attraction import Natural_Attraction
-=======
 from .amenity import Amenity
 from .park_amenity import Park_Amenity
 from .user import User
 from .user_favorite import User_Favorite
->>>>>>> main
+from .blog import Blog
