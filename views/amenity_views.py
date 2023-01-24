43c9b636b7befa387e78c33e104ecb662e5b893d
JohnMDoll <johnmdoll3@gmail.com>
2023-01-24 10:53:43 -0600

    refactored amenity and user GET functions to be more concise

--- views/amenity_views.py
+++ views/amenity_views.py
@@ -1,96 +1,85 @@
 import sqlite3
 from models import Amenity, Park_Amenity
+from sql_helper import get_all, get_single
 
 def get_all_amenity_types():
     """fetches list of amenity types from amenities table"""
-    with sqlite3.connect("./national_park.sqlite3") as conn:
-        conn.row_factory = sqlite3.Row
-        db_cursor = conn.cursor()
-
-        # Write the SQL query to get the information you want
-        db_cursor.execute(
-            """
-        SELECT
-            *
-        FROM Amenities
+    sql = (
         """
-        )
+        SELECT
+            a.id,
+            a.type
+        FROM Amenities a
+        """
+    )
 
-        amenity_types = []
-        dataset = db_cursor.fetchall()
+    amenity_types = []
+    dataset = get_all(sql)
 
-        for row in dataset:
-            amenity = Amenity(row["id"], row["type"])
+    for row in dataset:
+        amenity = Amenity(row["id"], row["type"])
 
-            amenity_types.append(amenity.__dict__)
+        amenity_types.append(amenity.__dict__)
 
     return amenity_types
 
 def get_amenity_type_by_id(id):
     """fetches list of amenity types from amenities table"""
-    with sqlite3.connect("./national_park.sqlite3") as conn:
-        conn.row_factory = sqlite3.Row
-        db_cursor = conn.cursor()
-
-        # Write the SQL query to get the information you want
-        db_cursor.execute(
-            """
+    sql = (
+        """
         SELECT
-            *
-        FROM Amenities
+            a.id,
+            a.type
+        FROM Amenities a
         WHERE id = ?
         """, (id, )
-        )
+    )
 
-        amenity = {}
-        data = db_cursor.fetchone()
-        amenity = Amenity(data["id"], data["type"])
+    amenity = {}
+    data = get_single(sql, id)
+    amenity = Amenity(data["id"], data["type"])
 
     return amenity.__dict__
 
 def get_all_amenities():
     """fetches list of amenity types from amenities table"""
-    with sqlite3.connect("./national_park.sqlite3") as conn:
-        conn.row_factory = sqlite3.Row
-        db_cursor = conn.cursor()
-
-        # Write the SQL query to get the information you want
-        db_cursor.execute(
-            """
-        SELECT
-            *
-        FROM Park_Amenities
+    sql = (
         """
-        )
+        SELECT
+            pa.id,
+            pa.name,
+            pa.amenity_id,
+            pa.park_id
+        FROM Park_Amenities pa
+        """
+    )
 
-        amenities = []
-        dataset = db_cursor.fetchall()
+    amenities = []
+    dataset = get_all(sql)
 
-        for row in dataset:
-            park_amenity = Park_Amenity(row["id"], row["name"], row["amenity_id"], row["park_id"])
+    for row in dataset:
+        park_amenity = Park_Amenity(row["id"], row["name"], row["amenity_id"], row["park_id"])
 
-            amenities.append(park_amenity.__dict__)
+        amenities.append(park_amenity.__dict__)
 
     return amenities
 
 def get_amenity_by_id(id):
     """fetches list of amenity types from amenities table"""
-    with sqlite3.connect("./national_park.sqlite3") as conn:
-        conn.row_factory = sqlite3.Row
-        db_cursor = conn.cursor()
-
-        # Write the SQL query to get the information you want
-        db_cursor.execute(
-            """
+    sql = (
+        """
         SELECT
-            *
-        FROM Park_Amenities
+            pa.id,
+            pa.name,
+            pa.amenity_id,
+            pa.park_id
+        FROM Park_Amenities pa
         WHERE id = ?
         """, (id, )
-        )
+    )
 
-        park_amenity = {}
-        data = db_cursor.fetchone()
-        park_amenity = Park_Amenity(data["id"], data["name"], data["amenity_id"], data["park_id"])
+    park_amenity = {}
+    data = get_single(sql, id)
+    park_amenity = Park_Amenity(data["id"], data["name"], data["amenity_id"], data["park_id"])
 
     return park_amenity.__dict__
