8cb6b24715f7c632fbd45283f2664408e759de14
Vanessa Spear <vanessavspear@gmail.com>
2023-01-24 10:56:48 -0600

    Updated events, event_registration, campgrounds, campground_reservation, and wildlife_views request modules to use the get_all() and get_single() helper functions from the sql_helper function module.

--- views/event_registration_requests.py
+++ views/event_registration_requests.py
@@ -1,31 +1,27 @@
-import sqlite3
 from models import Registration
+from sql_helper import get_all, get_single
 
 def get_all_registration():
     """Gets all event registration
     Returns:
         list: All event registration dictionaries"""
-    with sqlite3.connect("./national_park.sqlite3") as conn:
-        conn.row_factory = sqlite3.Row
-        db_cursor = conn.cursor()
+    sql = """
+            SELECT
+                reg.id,
+                reg.event_id,
+                reg.user_id
+            FROM event_registration reg
+            """
 
-        db_cursor.execute("""
-        SELECT
-            reg.id,
-            reg.event_id,
-            reg.user_id
-        FROM event_registration reg
-        """)
+    registrations = []
 
-        registrations = []
+    dataset = get_all(sql)
 
-        dataset = db_cursor.fetchall()
+    for row in dataset:
 
-        for row in dataset:
+        registration = Registration(row['id'], row['event_id'], row['user_id'])
 
-            registration = Registration(row['id'], row['event_id'], row['user_id'])
-
-            registrations.append(registration.__dict__)
+        registrations.append(registration.__dict__)
 
     return registrations
 
@@ -38,22 +34,18 @@ def get_single_registration(id):
     Returns:
         dict: event registration dictionary
     """
-    with sqlite3.connect("./national_park.sqlite3") as conn:
-        conn.row_factory = sqlite3.Row
-        db_cursor = conn.cursor()
+    sql = """
+            SELECT
+                reg.id,
+                reg.event_id,
+                reg.user_id
+            FROM event_registration reg
+            WHERE reg.id = ?
+            """
 
-        db_cursor.execute("""
-        SELECT
-            reg.id,
-            reg.event_id,
-            reg.user_id
-        FROM event_registration reg
-        WHERE reg.id = ?
-        """, ( id, ))
+    data = get_single(sql, id)
 
-        data = db_cursor.fetchone()
-
-        registration = Registration(data['id'], data['event_id'], data['user_id'])
+    registration = Registration(data['id'], data['event_id'], data['user_id'])
 
     return registration.__dict__
 
--- views/wildlife_views.py
+++ views/wildlife_views.py
@@ -1,59 +1,55 @@
 import sqlite3
 from models import Wildlife
+from sql_helper import get_all, get_single
 
 
 def get_all_wildlife():
     """Function to retrieve all wildlife from a database and return the data as a list of dictionaries"""
-    with sqlite3.connect("./national_park.sqlite3") as conn:
-        conn.row_factory = sqlite3.Row
-        db_cursor = conn.cursor()
+    sql = """
+            SELECT 
+            w.id,
+            w.name,
+            w.information,
+            w.wildlife_group_id,
+            w.image
+            FROM wildlife w
+            """
 
-        db_cursor.execute("""
-        SELECT 
-        w.id,
-        w.name,
-        w.information,
-        w.wildlife_group_id,
-        w.image
-        FROM wildlife w
-        """)
+    all_wildlife = []
+    
+    dataset = get_all(sql)
 
-        all_wildlife = []
-        dataset = db_cursor.fetchall()
+    for row in dataset:
+        wildlife = Wildlife(row['id'], row['name'], row['information'],
+                            row['wildlife_group_id'], row['image'])
 
-        for row in dataset:
-            wildlife = Wildlife(row['id'], row['name'], row['information'],
-                                row['wildlife_group_id'], row['image'])
-
-            all_wildlife.append(wildlife.__dict__)
+        all_wildlife.append(wildlife.__dict__)
 
     return all_wildlife
 
 
 def get_single_wildlife_type(id):
     """A function that retrieves a single wildlife type based on the given id"""
-    with sqlite3.connect("./national_park.sqlite3") as conn:
-        conn.row_factory = sqlite3.Row
-        db_cursor = conn.cursor()
+    sql = """
+            SELECT 
+            w.id,
+            w.name,
+            w.information,
+            w.wildlife_group_id,
+            w.image
+            FROM wildlife w
+            WHERE w.id = ?
+            """
 
-        db_cursor.execute("""
-        SELECT 
-        w.id,
-        w.name,
-        w.information,
-        w.wildlife_group_id,
-        w.image
-        FROM wildlife w
-        WHERE w.id = ?
-        """, (id, ))
+    data = get_single(sql, id)
+    
+    if not data:
+        return {}
 
-        data = db_cursor.fetchone()
-        if not data:
-            return {}
-        wildlife = Wildlife(data['id'], data['name'], data['information'],
-                            data['wildlife_group_id'], data['image'])
+    wildlife = Wildlife(data['id'], data['name'], data['information'],
+                        data['wildlife_group_id'], data['image'])
 
-        return wildlife.__dict__
+    return wildlife.__dict__
 
 
 def get_wildlife_by_park_id(park_id):
@@ -79,8 +75,10 @@ def get_wildlife_by_park_id(park_id):
         all_wildlife = []
 
         dataset = db_cursor.fetchall()
+
         if not dataset:
             return []
+
         if len(dataset) > 0:
             for row in dataset:
                 wildlife = Wildlife(row['id'], row['name'], row['information'],
@@ -88,4 +86,5 @@ def get_wildlife_by_park_id(park_id):
 
                 result = wildlife.__dict__
                 all_wildlife.append(result)
+
     return all_wildlife
