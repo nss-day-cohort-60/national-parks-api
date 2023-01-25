d271439f8d9897b9d00e98a15b68733f54aa043f
JohnMDoll <johnmdoll3@gmail.com>
2023-01-25 10:46:34 -0600

    "fixed method mapper bugs after main merge"

--- method_mapper.py
+++ method_mapper.py
@@ -1,4 +1,4 @@
-from views import get_all_amenities, get_all_amenity_types, get_amenity_by_id, get_amenity_type_by_id, get_all_blogs, get_single_blog, get_blogs_by_user_id, get_blogs_by_park_id, create_blog, update_blog, get_all_wildlife, get_single_wildlife_type, get_wildlife_by_park_id, create_wildlife, update_wildlife, delete_wildlife, get_all_natural_attractions, get_single_natural_attraction, get_natural_attraction_by_park_id, create_natural_attraction, update_natural_attraction, delete_natural_attraction
+from views import get_all_amenities, get_all_amenity_types, get_amenity_by_id, get_amenity_type_by_id, get_all_blogs, get_single_blog, get_blogs_by_user_id, get_blogs_by_park_id, create_blog, update_blog, get_all_wildlife, get_single_wildlife_type, get_wildlife_by_park_id, create_wildlife, update_wildlife, delete_wildlife, get_all_natural_attractions, get_single_natural_attraction, get_natural_attraction_by_park_id, create_natural_attraction, update_natural_attraction, delete_natural_attraction, get_all_parks, get_all_photos, get_all_user_favorites, get_all_users, get_single_park, get_single_photo, get_user_by_id, get_user_favorite_by_id, create_user, create_user_favorite
 
 method_mapper = {
     "amenities": {
@@ -42,7 +42,7 @@ method_mapper = {
         "park_id": get_wildlife_by_park_id,
         "post": create_wildlife,
         "put": update_wildlife,
-        "delete": delete_wildlife    
+        "delete": delete_wildlife
     },
     "natural_attractions": {
         "all": get_all_natural_attractions,
